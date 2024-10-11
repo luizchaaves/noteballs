65244bf93612210a6f2b564b5eca016ee85c58a6
Luiz Otavio <locchavescarvalho@gmail.com>
2024-10-11 18:32:37 -0300

    feat: create stats route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import Notes from '@/views/notes.vue'
+import Stats from '@/views/stats.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'notes',
         component: Notes,
+    },
+    {
+        path: '/stats',
+        name: 'stats',
+        component: Stats,
     },
 ]
 
