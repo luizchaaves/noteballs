7e541ce92e9c6a5b0464eb321c07e3bce335f0a9
Luiz Otavio <locchavescarvalho@gmail.com>
2024-10-12 01:38:58 -0300

    feat: insert cards and textarea into the notes component

--- src/views/notes.vue
+++ src/views/notes.vue
@@ -1,1 +1,38 @@
-<template>notes</template>
+<template>
+    <div class="notes">
+        <div class="card has-background-success-dark p-4 mb-5">
+            <div class="field">
+                <div class="control">
+                    <textarea
+                        class="textarea"
+                        placeholder="Adicione uma nova nota"
+                    />
+                </div>
+            </div>
+
+            <div class="field is-grouped is-grouped-right">
+                <div class="control">
+                    <button class="button is-link has-background-success">
+                        Adicionar nova nota
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div v-for="n in 3" class="card mb-4">
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Perferendis deserunt temporibus, itaque, obcaecati totam quo
+                    reprehenderit consequatur eaque distinctio quaerat
+                    laboriosam sit earum placeat consequuntur ea illum
+                    praesentium voluptas illo.
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    </div>
+</template>
