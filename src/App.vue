8271e0ad62983470e1caba267418e745d0227aa0
Luiz Otavio <locchavescarvalho@gmail.com>
2024-10-12 01:21:03 -0300

    feat: create desktop and mobile navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
+    <Navbar />
     <RouterView />
 </template>
+
+<script setup>
+import Navbar from '@/components/layout/navbar.vue'
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
